23fa81e50371c42bf4e3b5bbd899596a7d8d1bb1
f-gilak <63143804+f-gilak@users.noreply.github.com>
2022-08-19 17:17:15 +0430

    spring boot

---
+++ src/test/java/com/fariba/springBootDemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fariba.springBootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
