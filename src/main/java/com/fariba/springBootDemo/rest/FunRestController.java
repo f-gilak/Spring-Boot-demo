e0609c8e4d8c26ec68d46022bfec6983a48e6732
f-gilak <63143804+f-gilak@users.noreply.github.com>
2022-08-21 20:02:38 +0430

    spring boot: properties

--- src/main/java/com/fariba/springBootDemo/rest/FunRestController.java
+++ src/main/java/com/fariba/springBootDemo/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.fariba.springBootDemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,12 @@ import java.time.LocalDateTime;
 
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
 
     @GetMapping("/")
     public String sayHello() {
@@ -21,5 +28,10 @@ public class FunRestController {
     @GetMapping("/fortune")
     public String getDailyFortune() {
         return "Today is your locky dayyy!";
+    }
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team: " + teamName;
     }
 }
